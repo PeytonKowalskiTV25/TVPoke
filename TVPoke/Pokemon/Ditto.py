5472e07214e863dd437f0f3a5aa4c36fffdf4624
deantaylorTV2028 <daltacc73@gmail.com>
2025-04-17 08:20:27 -0700

    ditto thing yes (#31)
    Co-authored-by: CodyCundiffTV2025 <cody.w.cundiff@gmail.com>

---
+++ TVPoke/Pokemon/Ditto.py
@@ -0,0 +1,12 @@
+from TVPoke.BaseClasses.PokeTypes import Normal
+from TVPoke.BaseClasses.Move import Move
+
+class Ditto(Normal):
+    def __init__(self):
+        moves = [
+            Move("Transform", "NORMAL", 0),
+            Move("Hyper Beam", "NORMAL", 150),  
+            Move("Body Slam", "NORMAL", 85),   
+            Move("Double-Edge", "NORMAL", 120),     
+        ]
+        super().__init__("ditto", 48, moves, "./TVPoke/Pokemon/imgs/ditto.webp")
